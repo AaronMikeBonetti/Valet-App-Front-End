e1066063cb92cc4e9d4993623b970d54dc3c745b
Aaron Bonetti <AaronMik Bonetti@gmail.com>
2019-11-29 16:11:17 -0700

    finished login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Store from './Store'
+import LoginPage from './components/Login Page/LoginPage'
 
 export default function App() {
   return (
-    
     <Store>
       <div className='App'>
-      
+        <LoginPage> </LoginPage>
       </div>
     </Store>
     
--- src/components/Login Page/LoginPage.js
+++ src/components/Login Page/LoginPage.js
@@ -3,8 +3,15 @@ import './login-page.css'
 
 export default function LoginPage() {
     return (
-        <div className='login-page-container'>
-            
-        </div>
+        <section className='login-page__container'>
+            <div className='login-page__logo'></div>
+            <form className='login-page__form__container'>
+                    <i className='login-page__form__user-icon'></i>
+                    <i className='login-page__form__password-icon'></i>
+                    <input className='login-page__form__username' placeholder='USERNAME'></input>
+                    <input className='login-page__form__password' placeholder='PASSWORD'></input>
+                    <button className='login-page__form__submit-button'>Login</button>
+            </form>
+        </section>
     )
 }
